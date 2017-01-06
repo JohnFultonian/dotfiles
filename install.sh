91b7377b0ebc0d1912788cb36534da5d29efea43
John Fulton <john@john-fulton.com>
2017-01-06 11:11:41 +1100

    Added automatic setup of tmux powerline

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "This will replace existing .vimrc, .bash_profile, .bashrc, .tmux.conf and .git."
+echo "If you have python installed, will also download powerline and replace the tmux powerline config"
 read -p "Are you sure? " -n 1 -r
 echo
 if [[ ! $REPLY =~ ^[Yy]$ ]]
@@ -31,3 +32,13 @@ echo "Downloading powerline fonts"
 
 git clone https://github.com/powerline/fonts.git
 ./fonts/install.sh
+
+command -v pip >/dev/null 2>&1
+if [ $? -ne 0]; then
+  echo "Could not find python package manager. Skipping powerline setup"
+else
+  echo "Python install found. Setting up powerline..."
+  pip install --user powerline-status
+  PYTHON_PACKAGES=$(python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())")
+  ln -sf $SCRIPTPATH/powerline/tmux/default.json $PYTHON_PACKAGES/powerline/config_files/themes/tmux/default.json
+fi
